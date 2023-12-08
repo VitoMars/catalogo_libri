a2c7fc80b8c16693106cdefcfc1e478d89b22955
VitoMars <vito95v@gmail.com>
2023-12-08 16:26:49 +0100

    cambio nome variabile

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://openlibrary.org/people/mekBot/books/currently-reading.json';
+const BOOKS_URL = 'https://openlibrary.org/people/mekBot/books/currently-reading.json';
 
 export default {
    async getBooks() {
       try {
-         const response = await axios.get(BASE_URL);
+         const response = await axios.get(BOOKS_URL);
          return response.data;
       } catch (error) {
          console.error('Errore durante la richiesta dei libri:', error);
