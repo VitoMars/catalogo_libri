0a18e4069d3882f69ae1f399612fde1d6f4cbf8a
VitoMars <vito95v@gmail.com>
2023-12-08 10:57:38 +0100

    Implementazione modifica libro

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,21 +3,6 @@
       <!-- Caricamento visivo per l'utente -->
       <v-progress-linear v-if="loading" indeterminate />
 
-      <!-- Modulo di inserimento -->
-      <v-dialog v-model="addBookDialog" max-width="500">
-         <v-card>
-            <v-card-title>Aggiungi nuovo libro</v-card-title>
-            <v-card-text>
-               <v-form @submit.prevent="addBook">
-                  <v-text-field v-model="newBook.title" label="Titolo" required></v-text-field>
-                  <v-text-field v-model="newBook.author" label="Autore" required></v-text-field>
-                  <v-text-field v-model="newBook.publish_year" label="Anno di pubblicazione"></v-text-field>
-                  <v-btn type="submit" color="primary">Aggiungi libro</v-btn>
-               </v-form>
-            </v-card-text>
-         </v-card>
-      </v-dialog>
-
       <!-- Lista dei libri -->
       <v-list>
          <v-list-item-group v-if="books.length">
@@ -26,6 +11,11 @@
                   <v-list-item-title>{{ book.title }}</v-list-item-title>
                   <v-list-item-subtitle>{{ book.author }} - {{ book.publish_year }}</v-list-item-subtitle>
                </v-list-item-content>
+               <v-list-item-action>
+                  <v-btn @click="openEditBookDialog(book)" icon>
+                     <v-icon color="blue">mdi-pencil</v-icon>
+                  </v-btn>
+               </v-list-item-action>
                <v-list-item-action>
                   <v-btn @click="openDeleteBookDialog(book)" icon>
                      <v-icon color="red">mdi-delete</v-icon>
@@ -38,7 +28,37 @@
          </v-alert>
       </v-list>
 
-      <!-- Dialog di conferma eliminazione -->
+      <!-- Dialog per l'inserimento del libro -->
+      <v-dialog v-model="addBookDialog" max-width="500">
+         <v-card>
+            <v-card-title>Aggiungi nuovo libro</v-card-title>
+            <v-card-text>
+               <v-form @submit.prevent="addBook">
+                  <v-text-field v-model="newBook.title" label="Titolo" required />
+                  <v-text-field v-model="newBook.author" label="Autore" required />
+                  <v-text-field v-model="newBook.publish_year" label="Anno di pubblicazione" />
+                  <v-btn type="submit" color="primary">Aggiungi libro</v-btn>
+               </v-form>
+            </v-card-text>
+         </v-card>
+      </v-dialog>
+
+      <!-- Dialog di modifica libro -->
+      <v-dialog v-model="editBookDialog" max-width="500">
+         <v-card>
+            <v-card-title>Modifica libro</v-card-title>
+            <v-card-text>
+               <v-form @submit.prevent="editBook">
+                  <v-text-field v-model="bookToEdit.title" label="Titolo" required />
+                  <v-text-field v-model="bookToEdit.author" label="Autore" required />
+                  <v-text-field v-model="bookToEdit.publish_year" label="Anno di pubblicazione" />
+                  <v-btn @click="editBook" type="submit" color="primary">Salva modifiche</v-btn>
+               </v-form>
+            </v-card-text>
+         </v-card>
+      </v-dialog>
+
+      <!-- Dialog di eliminazione libro -->
       <v-dialog v-model="deleteBookDialog" max-width="400" persistent>
          <v-card class="px-1 pb-2">
             <v-card-title>Conferma eliminazione</v-card-title>
@@ -56,9 +76,7 @@
       <!-- Pulsante per aggiungere un libro -->
       <div class="d-flex justify-end mx-2">
          <v-btn @click="openAddBookDialog" fab color="primary">
-            <v-icon>
-               mdi-plus
-            </v-icon>
+            <v-icon>mdi-plus</v-icon>
          </v-btn>
       </div>
 
@@ -74,8 +92,14 @@ export default {
          loading: false,
          books: [],
          addBookDialog: false,
+         editBookDialog: false,
          deleteBookDialog: false,
          newBook: {
+            title: '',
+            author: '',
+            publish_year: '',
+         },
+         bookToEdit: {
             title: '',
             author: '',
             publish_year: '',
@@ -84,9 +108,14 @@ export default {
       }
    },
    methods: {
-      // Metodo per aprire il modulo di inserimento
+      // Metodo per aprire la dialog di inserimento libro
       openAddBookDialog() {
          this.addBookDialog = true;
+      },
+      // Metodo per aprire la dialog per modificare il libro
+      openEditBookDialog(book) {
+         this.bookToEdit = { ...book };
+         this.editBookDialog = true;
       },
       // Metodo per aprire la dialog di conferma eliminazione
       openDeleteBookDialog(book) {
@@ -110,7 +139,25 @@ export default {
             publish_year: '',
          };
       },
-      // Metodo per confermare l'eliminazione del libro
+      // Metodo per modificare un libro
+      editBook() {
+         if (this.bookToEdit) {
+            const index = this.books.findIndex(book => book.id === this.bookToEdit.id);
+
+            if (index !== -1) {
+               this.books.splice(index, 1, { ...this.bookToEdit });
+            }
+         }
+
+         this.editBookDialog = false;
+
+         this.bookToEdit = {
+            title: '',
+            author: '',
+            publish_year: '',
+         };
+      },
+      // Metodo per elimianre un libro
       deleteBook() {
          if (this.bookToDelete) {
             const index = this.books.findIndex(book => book.id === this.bookToDelete.id);
@@ -124,6 +171,7 @@ export default {
          this.bookToDelete = null;
       },
    },
+   // Recupero dati da API alla creazione del componente
    async created() {
       try {
          this.loading = true;
